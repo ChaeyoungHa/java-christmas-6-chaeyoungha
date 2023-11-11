dfb706602024904d3e5935e0ac6fac7a870fee7f
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-12 06:02:08 +0900

    fix: fix error message

--- src/main/java/christmas/exception/ErrorType.java
+++ src/main/java/christmas/exception/ErrorType.java
@@ -1,9 +1,8 @@
 package christmas.exception;
 
 public enum ErrorType {
-    INVALID_DATE_INPUT("방문할 날짜는 1 이상 31 이하의 숫자여야 합니다."),
-    INVALID_MENU_INPUT("주문하실 메뉴와 개수는 <한글 메뉴이름>-<숫자> 형식이고, 쉼표를 통해 구분되어야 합니다."),
-    NOT_FOUND_MENU("존재하지 않는 메뉴입니다."),
+    INVALID_DATE_INPUT("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    INVALID_MENU_INPUT("유효하지 않은 주문입니다. 다시 입력해 주세요."),
     ;
 
     private final String message;
