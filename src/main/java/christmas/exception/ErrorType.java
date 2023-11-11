66885437bcc0bd72c0c41c67dcbf1e520a1b2e4f
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-12 05:45:40 +0900

    chore: fix typo

--- src/main/java/christmas/exception/ErrorType.java
+++ src/main/java/christmas/exception/ErrorType.java
@@ -1,7 +1,7 @@
 package christmas.exception;
 
 public enum ErrorType {
-    INVALID_DATE_INPUT("방문할 날짜는 1이상 31 이상의 숫자여야 합니다."),
+    INVALID_DATE_INPUT("방문할 날짜는 1 이상 31 이하의 숫자여야 합니다."),
     INVALID_MENU_INPUT("주문하실 메뉴와 개수는 <한글 메뉴이름>-<숫자> 형식이고, 쉼표를 통해 구분되어야 합니다."),
     NOT_FOUND_MENU("존재하지 않는 메뉴입니다."),
     ;
