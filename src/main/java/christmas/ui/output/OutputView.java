7187b430d6f3ffb7da18645321f0c20945358c78
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-12 02:59:42 +0900

    feat: print HELLO message at the beginning

---
+++ src/main/java/christmas/ui/output/OutputView.java
@@ -0,0 +1,8 @@
+package christmas.ui.output;
+
+public class OutputView {
+    private static final String HELLO = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
+    public static void printHello() {
+        System.out.println(HELLO);
+    }
+}
