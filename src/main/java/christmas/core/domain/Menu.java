d893a494bdf06f1c82adebc18b181f8bfd8a1b52
ChaeyoungHa <hcy1722@korea.ac.kr>
2023-11-15 01:08:28 +0900

    refactor: remove unused function

--- src/main/java/christmas/core/domain/Menu.java
+++ src/main/java/christmas/core/domain/Menu.java
@@ -1,24 +1,20 @@
 package christmas.core.domain;
 
-import christmas.exception.ErrorType;
-import christmas.exception.ReservationException;
-
-import java.util.Arrays;
-import java.util.Objects;
+import static christmas.core.domain.MenuCategory.*;
 
 public enum Menu {
-    BUTTON_MUSHROOM_SOUP("양송이수프", MenuCategory.APPETIZER, 6000),
-    TAPAS("타파스", MenuCategory.APPETIZER,5500),
-    CAESAR_SALAD("시저샐러드", MenuCategory.APPETIZER, 8000),
-    T_BONE_STEAK("티본스테이크", MenuCategory.MAIN, 55000),
-    BARBECUE_RIBS("바비큐립", MenuCategory.MAIN, 54000),
-    SEAFOOD_PASTA("해산물파스타", MenuCategory.MAIN, 35000),
-    CHRISTMAS_PAST("크리스마스파스타", MenuCategory.MAIN, 25000),
-    CHOCOLATE_CAKE("초코케이크", MenuCategory.DESSERT, 15000),
-    ICECREAM("아이스크림", MenuCategory.DESSERT, 5000),
-    ZERO_COKE("제로콜라", MenuCategory.DRINK, 3000),
-    RED_WINE("레드와인", MenuCategory.DRINK, 60000),
-    CHAMPAGNE("샴페인", MenuCategory.DRINK, 25000);
+    BUTTON_MUSHROOM_SOUP("양송이수프", APPETIZER, 6000),
+    TAPAS("타파스", APPETIZER,5500),
+    CAESAR_SALAD("시저샐러드", APPETIZER, 8000),
+    T_BONE_STEAK("티본스테이크", MAIN, 55000),
+    BARBECUE_RIBS("바비큐립", MAIN, 54000),
+    SEAFOOD_PASTA("해산물파스타", MAIN, 35000),
+    CHRISTMAS_PAST("크리스마스파스타", MAIN, 25000),
+    CHOCOLATE_CAKE("초코케이크", DESSERT, 15000),
+    ICECREAM("아이스크림", DESSERT, 5000),
+    ZERO_COKE("제로콜라", DRINK, 3000),
+    RED_WINE("레드와인", DRINK, 60000),
+    CHAMPAGNE("샴페인", DRINK, 25000);
 
     private final String name;
     private final MenuCategory menuCategory;
@@ -36,13 +32,6 @@ public enum Menu {
 
     public int getPrice() {
         return price;
-    }
-
-    public static Menu of(String name) {
-        return Arrays.stream(Menu.values())
-                .filter(menu -> Objects.equals(menu.name, name))
-                .findFirst()
-                .orElseThrow(() -> new ReservationException(ErrorType.INVALID_MENU_INPUT));
     }
 
     public boolean hasCategoryOf(MenuCategory category) {
